5883be218f878b14311f97ed7a440ec89f2423e9
nyr <romashmarov82@gmail.com>
2025-04-27 23:53:10 +0300

    feat: build config

--- build.sh
+++ build.sh
@@ -3,19 +3,33 @@
 DEBUG_BUILD_DIR="build/debug/"
 RELEASE_BUILD_DIR="build/release/"
 BUILD_RELEASE=0
+CMAKE_OPTS="-DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_CXX_COMPILER=clang++"
+CMAKE_BUILD_OPTS="-j"
 
-if [ $# -gt 0 ] && [ "$1" = "-r" ] || [ "$1" = "--release" ]; then
-  BUILD_RELEASE=1
-fi
+for arg in "$@"; do
+  case "$arg" in
+  -r | --release)
+    BUILD_RELEASE=1
+    CMAKE_OPTS="-DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CXX_COMPILER=clang++"
+    ;;
+  -c | --clean)
+    echo "Performing cleanup..."
+    CMAKE_BUILD_OPTS+=" --clean-first"
+    ;;
+  *)
+    echo "Unknown argument: $arg"
+    ;;
+  esac
+done
 
-if [ $BUILD_RELEASE == 0 ]; then
+if [ $BUILD_RELEASE -eq 0 ]; then
   echo "Building in debug mode"
   [ -d "$DEBUG_BUILD_DIR" ] || mkdir "$DEBUG_BUILD_DIR"
   cd "$DEBUG_BUILD_DIR"
-  cmake -DCMAKE_BUILD_TYPE=DEBUG ../../ && cmake --build . -j
+  cmake $CMAKE_OPTS ../../ && cmake --build . $CMAKE_BUILD_OPTS
 else
   echo "Building in release mode"
   [ -d "$RELEASE_BUILD_DIR" ] || mkdir "$RELEASE_BUILD_DIR"
-  cd "$DEBUG_BUILD_DIR"
-  cmake -DCMAKE_BUILD_TYPE=RELEASE ../../ && cmake --build . -j
+  cd "$RELEASE_BUILD_DIR"
+  cmake $CMAKE_OPTS ../../ && cmake --build . $CMAKE_BUILD_OPTS
 fi
